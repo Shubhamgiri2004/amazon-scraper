267949a0443a56d56e4c50e65f30fc542a8a252e
SHUBHAM GIRI <shubhamgiriofficial03@gmail.com>
2025-09-14 12:28:05 +0530

    new route added

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import axios from "axios";
-import request  from "request";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
-const apiKey = '83bff2bbdd8b6f3ac0225ddd34afabb7';
+const apiKey = process.env.SCRAPER_API_KEY;
 const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("hello from melody team");
+});
+
 //Get product details
-app.get('/products/:productId', async(req, res)=>{
-    const {productId} = req.params;
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
 
   try {
     const response = await axios.get(
@@ -24,13 +30,38 @@ app.get('/products/:productId', async(req, res)=>{
   }
 });
 
+//Get product reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
 
-
-app.get("/", (req, res)=>{
-    res.send("hello from melody team");
+  try {
+    const response = await axios.get(
+      `${baseUrl}&url=http://amazon.com/product-reviews/${productId}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.json({
+      error: error.message,
+    });
+  }
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on the port ${PORT}`);
+// Get product offer
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await axios.get(
+      `${baseUrl}$url=http://amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.json({
+      error: error.message,
+    });
+  }
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on the port ${PORT}`);
+});
